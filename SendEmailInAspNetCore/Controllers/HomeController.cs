b25f297c2b4fce7edbc075e6593a7d445309539b
Bekenty Jean Baptiste <bekenty.baptiste@outlook.com>
2019-02-14 22:42:42 -0500

    checking latest working version of the code on how to send email in asp .net core using Mailkit library

--- SendEmailInAspNetCore/Controllers/HomeController.cs
+++ SendEmailInAspNetCore/Controllers/HomeController.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
 using SendEmailInAspNetCore.Models;
+using System;
+using System.Diagnostics;
 
 namespace SendEmailInAspNetCore.Controllers
 {
@@ -28,7 +28,47 @@ namespace SendEmailInAspNetCore.Controllers
 
             return View();
         }
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel contactViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //instantiate a new MimeMessage
+                    var message = new MimeMessage();
 
+                    //Setting the To e-mail address
+                    message.To.Add(new MailboxAddress("E-mail Recipient Name", "recipient@domain.com"));
+                    //Setting the From e-mail address
+                    message.From.Add(new MailboxAddress("E-mail From Name", "From@domain.com"));
+                    //E-mail subject 
+                    message.Subject = contactViewModel.Subject;
+                    //E-mail message body
+                    message.Body = new TextPart(TextFormat.Html)
+                    {
+                        Text = contactViewModel.Message + " Message was sent by: " + contactViewModel.Name + " E-mail: " + contactViewModel.Email
+                    };
+
+                    //Configure the e-mail
+                    using (var emailClient = new SmtpClient())
+                    {
+                        emailClient.Connect("smtp.gmail.com", 587, false);
+                        emailClient.Authenticate("emailaddress@gmail.com", "password");
+                        emailClient.Send(message);
+                        emailClient.Disconnect(true);
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    ModelState.Clear();
+                    ViewBag.Exception = $" Oops! Message could not be sent. Error:  {ex.Message}";
+                }
+
+            }
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ SendEmailInAspNetCore/Models/ContactViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SendEmailInAspNetCore.Models
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [StringLength(60, MinimumLength = 5)]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
