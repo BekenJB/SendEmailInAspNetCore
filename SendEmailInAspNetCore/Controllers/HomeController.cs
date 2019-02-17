343e53825e7d566833960f5961d2417d3aac4b92
Bekenty Jean Baptiste <bekenty.baptiste@outlook.com>
2019-02-17 11:44:39 -0500

    checked in latest code after added email template for render view into string

--- SendEmailInAspNetCore/Controllers/HomeController.cs
+++ SendEmailInAspNetCore/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MimeKit;
 using MimeKit.Text;
+using SendEmailInAspNetCore.Helpers;
 using SendEmailInAspNetCore.Models;
 using System;
 using System.Diagnostics;
@@ -35,26 +36,37 @@ namespace SendEmailInAspNetCore.Controllers
             {
                 try
                 {
+                    string emailBody = string.Empty;
+                    bool useEmailTemplate = true;
                     //instantiate a new MimeMessage
                     var message = new MimeMessage();
 
                     //Setting the To e-mail address
                     message.To.Add(new MailboxAddress("E-mail Recipient Name", "recipient@domain.com"));
                     //Setting the From e-mail address
-                    message.From.Add(new MailboxAddress("E-mail From Name", "From@domain.com"));
+                    message.From.Add(new MailboxAddress("E-mail From Name", "fromemail@domain.com"));
                     //E-mail subject 
                     message.Subject = contactViewModel.Subject;
                     //E-mail message body
+                    if (useEmailTemplate)
+                    {
+                        emailBody = ViewsToStringOutputHelper.RenderRazorViewToString(this, "Welcome", null);
+                    }
+                    else
+                    {
+                        emailBody = contactViewModel.Message + " Message was sent by: " + contactViewModel.Name + " E-mail: " + contactViewModel.Email;
+
+                    }
                     message.Body = new TextPart(TextFormat.Html)
                     {
-                        Text = contactViewModel.Message + " Message was sent by: " + contactViewModel.Name + " E-mail: " + contactViewModel.Email
+                        Text = emailBody
                     };
 
                     //Configure the e-mail
                     using (var emailClient = new SmtpClient())
                     {
                         emailClient.Connect("smtp.gmail.com", 587, false);
-                        emailClient.Authenticate("emailaddress@gmail.com", "password");
+                        emailClient.Authenticate("gmailaccount@gmail.com", "password");
                         emailClient.Send(message);
                         emailClient.Disconnect(true);
                     }
@@ -79,5 +91,10 @@ namespace SendEmailInAspNetCore.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+
     }
 }
---
+++ SendEmailInAspNetCore/Helpers/ViewsToStringOutputHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.IO;
+
+namespace SendEmailInAspNetCore.Helpers
+{
+    public class ViewsToStringOutputHelper
+    {
+
+        public static string RenderRazorViewToString(Controller controller, string viewName, object model)
+        {
+            controller.ViewData.ModelState.Clear();
+            controller.ViewData.Model = model;
+
+            using (var sw = new StringWriter())
+            {
+                ViewEngineResult viewResult = null;
+                var engine = controller.HttpContext.RequestServices.GetService(typeof(ICompositeViewEngine)) as ICompositeViewEngine;
+
+                viewResult = engine.FindView(controller.ControllerContext, viewName, false);
+
+                var viewContext = new ViewContext(controller.ControllerContext, viewResult.View, controller.ViewData, controller.TempData, sw, new HtmlHelperOptions());
+                viewResult.View.RenderAsync(viewContext);
+
+                return sw.GetStringBuilder().ToString();
+
+            }
+        }
+    }
+}
